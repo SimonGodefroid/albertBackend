50dccafa78569fe23b245b8e8f5be486fccc69b8
SimonGodefroid <simon.godefroid@gmail.com>
2017-03-22 12:10:04 +0100

    added category filter

--- routes/events.js
+++ routes/events.js
@@ -19,25 +19,13 @@ router.get('/all', function (req, res) {
 
 
 router.get('/', function (req, res,next) {
-  if(!req.query.categories){
+  if(!req.query.category){
     return next("Categories is mandatory");
   }
-  Person.
-  find({
-    occupation: /host/,
-    'name.last': 'Ghost',
-    age: { $gt: 17, $lt: 66 },
-    likes: { $in: ['vaporizing', 'talking'] }
-  }).
-  limit(10).
-  sort({ occupation: -1 }).
-  select({ name: 1, occupation: 1 }).
-  exec(callback);
-
   
   Event.find({
-    albertCat: { $in: [req.query.albertCat]},
-  }, function (err, events) { // on récupère dans mongodb via mongoose toutes les entrées dans un tableau.
+    albertCat: { $in: [parseInt(req.query.category)]},
+  }, function (err, events) {
     if (err) {
       console.log('An error occurred' + err);
     } else {
