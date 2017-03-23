2c49c34fd3e842bb765e4d29b9c2361e27ccdd40
SimonGodefroid <simon.godefroid@gmail.com>
2017-03-23 19:08:59 +0100

    sorted events to include in progress and upcoming

--- routes/events.js
+++ routes/events.js
@@ -47,10 +47,11 @@ router.get('/all', function (req, res) {
 });
 
 
-var currentTime = new Date();
-var today = new Date( currentTime.getFullYear(),currentTime.getMonth(),currentTime.getDate());
+
 
 router.get('/date', function (req, res,next) {
+  var currentTime = new Date();
+  var today = new Date( currentTime.getFullYear(),currentTime.getMonth(),currentTime.getDate());
   Event.
   find({
     'evenements.realDateStart': {$lte: today},
@@ -74,6 +75,8 @@ router.get('/date', function (req, res,next) {
 // http://localhost:3002/api/events/date/all
 
 router.get('/date/all', function (req, res,next) {
+  var currentTime = new Date();
+  var today = new Date( currentTime.getFullYear(),currentTime.getMonth(),currentTime.getDate());
   Event.
   find({})
   .sort({'evenements.realDateStart': 'ascending'})
@@ -100,42 +103,84 @@ router.get('/date/all', function (req, res,next) {
   });
 });
 
-
-
-
-
-
-router.get('/date/filters', function (req, res,next) {
-    Event.
-  find({
-    'evenements.realDateStart': {$lte: today},
-    'evenements.realDateEnd': {$gt: today},
-  })
-
-
-  
-  .sort({'evenements.realDateStart': 'ascending'})
-  .exec(function (err, events) {
-    if (err) {
-      console.log('An error occurred' + err);
-    } else {
-      var eventsDates=[];
-      for (var i = 0;i<events.length;i++){
-        var eventDate = {};
-        eventDate.realDateStart = events[i].evenements.realDateStart;
-        eventDate.realDateEnd = events[i].evenements.realDateEnd;
-        eventDate.title = events[i].title;
-        eventsDates.push(eventDate);
+router.get('/date/filters',
+  function(req,res,next){
+    var currentTime = new Date();
+    var today = new Date( currentTime.getFullYear(),currentTime.getMonth(),currentTime.getDate());
+    Event.find({
+      '$or':[{
+        'evenements.realDateStart': {$lte: today},
+        'evenements.realDateEnd': {$gt: today},
+      },{
+        'evenements.realDateStart': {$gte: today},
+      }]
+    })
+    .sort({'evenements.realDateStart': 'ascending'})
+    //.select('evenements.realDateStart')
+    .select(['evenements.realDateStart','evenements.realDateEnd','title'])
+    .exec(function (err, events) {
+      if (err) {
+        console.log('An error occurred' + err);
+      } else {
+        res.json({
+          events:events,
+          total: events.length
+        });
       }
-      console.log(eventsDates);
-      res.json({
-        events: eventsDates,
-        count: events.length
-      }); 
-    }
+    });
   });
-});
 
+// router.get('/date/filters',
+// function (req, res,next) {
+//   var currentTime = new Date();
+//   var today = new Date( currentTime.getFullYear(),currentTime.getMonth(),currentTime.getDate());
+//   Event.find({
+//     'evenements.realDateStart': {$lte: today},
+//     'evenements.realDateEnd': {$gt: today},
+//   })
+//   .sort({'evenements.realDateStart': 'ascending'})
+//   .exec(function (err, eventsInProgress) {
+//     if (err) {
+//       console.log('An error occurred' + err);
+//     } else {
+//       Event.find({
+//         'evenements.realDateStart': {$gte: today},
+//       })
+//       .sort({'evenements.realDateStart': 'ascending'})
+//       .exec(function(err,eventsComing){
+//         if (err) {
+//           console.log('An error occurred' + err);
+//         } else {
+//         var eventsInProgressList=[];
+//         for (var j=0; j<eventsInProgress.length;j++){
+//           var eventInProgress = {};
+//           eventInProgress.realDateStart = eventsInProgress[j].evenements.realDateStart;
+//           eventInProgress.realDateEnd = eventsInProgress[j].evenements.realDateEnd;
+//           eventInProgress.title = eventsInProgress[j].title;
+//           eventsInProgressList.push(eventInProgress);
+//         }
+//         var eventsComingList=[];
+//         for (var i = 0;i<eventsComing.length;i++){
+//           var eventComing = {};
+//           eventComing.realDateStart = eventsComing[i].evenements.realDateStart;
+//           eventComing.realDateEnd = eventsComing[i].evenements.realDateEnd;
+//           eventComing.title = eventsComing[i].title;
+//           eventsComingList.push(eventComing);
+//         }
+//         res.json({
+//           //events:eventsInProgressDates.concat(eventsComing),
+//           eventsInProgress:eventsInProgressList,
+//           totalInProgress: eventsInProgress.length,
+//           eventsComing: eventsComingList,
+//           totalComing: eventsComing.length,
+//           events:eventsInProgressList.concat(eventsComingList),
+//           total: eventsInProgress.length + eventsComingList.length
+//         });
+//         }
+//       });
+//     }
+//   });
+// });
 
 
 
